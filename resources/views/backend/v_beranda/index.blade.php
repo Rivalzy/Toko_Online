e13250552e710437f4b42a3d755d81532d938890
Rivalzy <rivalds011@gmail.com>
2025-04-23 09:17:28 +0700

    login user

---
+++ resources/views/backend/v_beranda/index.blade.php
@@ -0,0 +1,23 @@
+@extends('backend.v_layouts.app')
+@section('content')
+    <!-- contentAwal -->
+
+    <h3> {{ $judul }} </h3>
+    @if (Auth::check())
+        <p>
+            Selamat Datang, <b>{{ Auth::user()->nama }}</b> pada aplikasi Toko Online dengan hak akses yang anda miliki <br>
+            sebagai <b>
+                @if (Auth::user()->role == 1)
+                    Super Admin
+                @elseif (Auth::user()->role == 0)
+                    Admin
+                @else
+                    User
+                @endif
+            </b> ini adalah halaman utama dari aplikasi ini.
+        </p>
+    @else
+        <p>Anda belum login.</p>
+    @endif
+    <!-- contentAkhir -->
+@endsection
